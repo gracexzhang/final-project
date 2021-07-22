311b36c53ca5cb753681f265db22d3ee7fbf5b6e
mkoster10 <mokoster11@gmail.com>
2021-07-22 17:42:07 +0000

    day 14

--- app.py
+++ app.py
@@ -11,6 +11,18 @@ app = Flask(__name__)
 
 # -- Routes section --
 @app.route('/')
-@app.route('/index')
+@app.route('/index.html')
 def index():
-    return render_template("index_copy.html", time = datetime.now())
+    return render_template("index.html", time = datetime.now())
+
+@app.route('/left-sidebar.html')
+def leftsidebar():
+    return render_template("left-sidebar.html")
+
+@app.route('/right-sidebar.html')
+def rightsidebar():
+    return render_template("right-sidebar.html")
+
+@app.route('/no-sidebar.html')
+def nosidebar():
+    return render_template("no-sidebar.html")
---
+++ matplot.py
@@ -0,0 +1,6 @@
+
+import matplotlib 
+import yfinance as yf
+data = pdr.get_data_yahoo("SPY", start="2017-01-01", end="2017-04-30") 
+
+
