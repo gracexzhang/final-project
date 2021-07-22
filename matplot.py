67675c183e4a53b9457d824c5a6dd2af066c458b
mkoster10 <mokoster11@gmail.com>
2021-07-22 17:52:24 +0000

    matplot

--- matplot.py
+++ matplot.py
@@ -1,6 +1,15 @@
 
 import matplotlib 
+import pandas as pd
 import yfinance as yf
-data = pdr.get_data_yahoo("SPY", start="2017-01-01", end="2017-04-30") 
 
+# data = pdr.get_data_yahoo("SPY", start="2017-01-01", end="2017-04-30") 
+
+
+
+data = yf.download("AAPL", start="2017-01-01", end="2017-05-30")
+
+
+
+print (data.pandas.DataFrame)
 
