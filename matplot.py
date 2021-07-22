544d6620dfafbf1d0e62c0e8cee51116da352ddc
mkoster10 <mokoster11@gmail.com>
2021-07-22 18:44:15 +0000

    matplot updates

--- matplot.py
+++ matplot.py
@@ -1,15 +1,34 @@
 
 import matplotlib 
+import numpy as np
 import pandas as pd
 import yfinance as yf
-
+import matplotlib.pyplot as plt 
 # data = pdr.get_data_yahoo("SPY", start="2017-01-01", end="2017-04-30") 
 
 
 
-data = yf.download("AAPL", start="2017-01-01", end="2017-05-30")
+# data = yf.download("AAPL", start="2017-01-01", end="2017-05-30")
+
+msft = yf.Ticker("MSFT").history()
+
+# msft_dictionary = dict.fromkeys(msft, "Price")
+
+# msft_date = msft.drop(columns=['Close','Open', 'High',"Low","Volume","Dividends","Stock Splits"])
+# msft_price = msft.drop(columns=["",'Open', 'High',"Low","Volume","Dividends","Stock Splits"])
 
 
 
-print (data.pandas.DataFrame)
+msft_close = msft.drop(columns=['Open', 'High',"Low","Volume","Dividends","Stock Splits"])
+print (msft_close.plot)
+#date = data["date"]
+# close = data ["close"]
+# plt.plot(msft_date, msft_price)
+plt.xlabel('Time (day)')
+plt.ylabel('Price ($)')
+
+
+
+
+
 
