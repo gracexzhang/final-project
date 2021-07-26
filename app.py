3bf10f8ef10e4ace55d3f912d4aadf3b8b98d091
Grace Zhang <gracezhang0423@gmail.com>
2021-07-26 18:52:38 +0000

    ..

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template, request
 from datetime import datetime
 # from model import getImageUrlFrom
-import numpy as np
+# import numpy as np
 # import pandas as pd
 # import yfinance as yf
 # import base64
